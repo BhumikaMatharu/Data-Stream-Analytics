d0ed4e8365c998c85f607d6e2389c62f21fea8cb
Bhumika Matharu <bhumika.matharu@gmail.com>
2021-02-21 03:52:01 -0800

    Handle Edge Cases

--- process_faust.py
+++ process_faust.py
@@ -1,13 +1,8 @@
 import faust
 
-# Creating the faust App and the consumer group
-app = faust.App('cisco1', broker='kafka://localhost:9092', value_serializer='json')
+# Creating the faust App and assigning the consumer group to consume from Kafka topic
+app = faust.App('test1', broker='kafka://localhost:9092', value_serializer='json')
 
-# Input
-class Stream(faust.Record):
-    device_id: int
-    value: int
-    timestamp: int
 
 # Output
 class Stats(faust.Record):
@@ -18,9 +13,10 @@ class Stats(faust.Record):
     avg: float
 
 
-# Topic and Table
-topic = app.topic('TestTopic2')
+# Initiate Faust Topic, Faust Table, Dictionaries
+topic = app.topic('TestTopic')
 table = app.Table('counter', partitions=1, default=list)
+processed_time, delayed_devices = dict(), dict()
 
 
 # Convert timestamp to nearest timestamp min
@@ -28,9 +24,10 @@ def convert_timestamp(time):
     time = int(time)
     return time - (time % 60)
 
+
 # On receiving the incoming stream
 @app.agent(topic)
-async def receive(stream):
+async def receiver(stream):
     last_timestamp = 0
     async for s in stream:
         device_id, value, timestamp = s.split(',')
@@ -38,18 +35,20 @@ async def receive(stream):
         value = int(value[7:])
         curr_timestamp = convert_timestamp(int(timestamp[12:]))
 
-        if (curr_timestamp-last_timestamp)==curr_timestamp:
-            last_timestamp = curr_timestamp
-            await eval_stats(device_id,value,last_timestamp)
-        elif (curr_timestamp-last_timestamp)!=0:
+        # For delayed timestamps
+        if (curr_timestamp - last_timestamp) < 0:
+            await handle_delay(device_id, value, curr_timestamp)
+            continue
+
+        if (curr_timestamp - last_timestamp) != 0 and (curr_timestamp - last_timestamp) != curr_timestamp:
             await avg_stats(last_timestamp)
 
         last_timestamp = curr_timestamp
-        await eval_stats(device_id,value,last_timestamp)
+        await eval_stats(device_id, value, last_timestamp)
 
 
 # Calculating min,max,count,sum for each device in each min
-async def eval_stats(id,val,time):
+async def eval_stats(id, val, time):
     device_id = id
     value = val
     curr_timestamp = time
@@ -63,16 +62,68 @@ async def eval_stats(id,val,time):
         sum = val[3] + value
         table[(device_id, curr_timestamp)] = [minm, maxm, count, sum]
 
+
 # Calculating average for each device in each min
 async def avg_stats(curr_minute):
-    for k, v in table.items():
-        if k[1] == curr_minute:
-            avg = v[3] / v[2]
-            await store_to_db([k[0],curr_minute,v[0],v[1],avg])
+    keys = [k for k in table.keys() if k[1] == curr_minute]
+    vals = [table[k] for k in table.keys() if k[1] == curr_minute]
+    for k, v in zip(keys, vals):
+        avg = v[3] / v[2]
+        await store_to_db([k[0], curr_minute, v[0], v[1], avg])
+    # processed_time['time'] = curr_minute
+    # print("Process_time", processed_time['time'])
+    # Deleting processed stats from table
+    for del_k in keys:
+        del table[del_k]
+
 
 # Pushing the processed stats to db of each device in each min
 async def store_to_db(out):
+    if 'time' not in processed_time.keys():
+        processed_time['time']=out[1]
+    else:
+        processed_time['time']=out[1]
     print("Store_DB", out)
+
+
+# To process last timestamp of data stream or Delay in data stream
+@app.timer(interval=60.0)
+async def per_min():
+    delayed_keys = [k for k in delayed_devices.keys()]
+    if delayed_keys:
+        for k, v in delayed_devices.items():
+            await store_to_db([k[0], k[1], v[0], v[1], v[4]])
+    for del_k in delayed_keys:
+        del delayed_devices[del_k]
+
+    #For last timestamp in Kafka Topic
+    keys = [k for k in table.keys() if (k[1] - processed_time['time']) >= 60]
+    if keys:
+        for k in keys:
+            print("per min", [k[1], processed_time['time']])
+            await process_last_timestamp(k[1])
+
+
+async def process_last_timestamp(time):
+    keys = [k for k in table.keys() if k[1] == time]
+    vals = [table[k] for k in table.keys() if k[1] == time]
+    for k, v in zip(keys, vals):
+        avg = v[3] / v[2]
+        await store_to_db([k[0], time, v[0], v[1], avg])
+
+
+# Handle delay of 1 minute(60 seconds)
+async def handle_delay(id, val, delay):
+    if (id, delay) not in delayed_devices.keys():
+        delayed_devices[(id, delay)] = [val, val, 1, val, float(val)]
+    else:
+        data = delayed_devices[(id, delay)]
+        minm = min(data[0], val)
+        maxm = max(data[1], val)
+        count = data[2] + 1
+        sum = data[3] + val
+        avg = sum / count
+        delayed_devices[(id, delay)] = [minm, maxm, count, sum, float(avg)]
 
 
 if __name__ == '__main__':
